4b7b54dfe3303907c43124a0c7d19c5568d71fd2
brendantwy <brendantan1998@gmail.com>
2022-05-04 05:08:40 +0800

    navbar under development

--- eezee-frontend-intern-take-home/src/App.js
+++ eezee-frontend-intern-take-home/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Home from './components/home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Home />
     </div>
   );
 }
--- eezee-frontend-intern-take-home/src/components/home/Home.js
+++ eezee-frontend-intern-take-home/src/components/home/Home.js
@@ -2,3 +2,17 @@ import { Layout, Row, Col } from 'antd';
 import Navbar from '../navbar/Navbar';
 
 const { Header, Content } = Layout;
+
+export default function Home() {
+    return (
+        <Layout className='layout'>
+            <Header>
+                <Row>
+                    <Navbar clasName='navbar' />
+                </Row>
+            </Header>
+            <Content>
+            </Content>
+        </Layout>
+    )
+}
